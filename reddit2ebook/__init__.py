2d51c47c134dab30d618300d7cd6451fe22ad877
Gerrit Helling <gerrit@akuma.pcitures>
2015-05-22 03:32:53 +0200

    Wrote most of the program

---
+++ reddit2ebook/__init__.py
@@ -0,0 +1,13 @@
+"""
+CLI Program to turn reddit submissions and comments into Ebooks
+
+ Usage: reddit2ebook initfile
+
+ The initfile should be in the following format:
+
+ bookname:
+     - url
+     - url
+     - url
+
+"""
---
+++ setup.py
@@ -0,0 +1,61 @@
+"""Convert reddit stories and comments into ebooks
+See:
+https://github.com/MissingNoIOI/reddit2ebook
+"""
+
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+    setup(
+        name='reddit2ebook',
+
+        version='1.0.0'
+
+    description = 'convert reddit stories and comments into ebooks',
+                  long_description = long_description,
+
+                                     url = 'https://github.com/MissingNoIOI/reddit2ebook',
+
+                                           author = 'Gerrit Helling',
+                                                    author_email = 'gerrit@akuma.pictures',
+
+                                                                   license = 'GPLv3',
+
+                                                                             classifiers = [
+
+                                                                                               'Development Status :: 2 - Beta',
+
+                                                                                               'Intended Audience :: End Users/Desktop'
+                                                                                               'Topic :: Multimedia'
+
+
+                                                                                               'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+
+                                                                                               'Programming Language :: Python :: 3',
+                                                                                               'Programming Language :: Python :: 3.2',
+                                                                                               'Programming Language :: Python :: 3.3',
+                                                                                               'Programming Language :: Python :: 3.4',
+                                                                                           ],
+
+                                                                                           keywords = 'reddit ebook epub pandoc offline',
+
+                                                                                                      packages = find_packages(
+        exclude=['contrib', 'docs', 'tests*']),
+
+                                                                                                                 install_requires = [
+                                                                                                                                        'pypandoc',
+                                                                                                                                        'praw',
+                                                                                                                                        'PyYaml'],
+
+                                                                                                                                    entry_points = {
+                                                                                                                                                       'console_scripts': [
+                                                                                                                                                           'reddit2ebook=reddit2ebook:main',
+                                                                                                                                                       ],
+                                                                                                                                                   },
+    )
